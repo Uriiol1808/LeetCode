8e69ef3c2405e24dc09bb3c78088f1c9bee5bc12
Uri <oriolmarin18@gmail.com>
2024-05-21 10:16:29 +0200

    c++ commit

--- Hash Map & Set/1207_unique_number_of_occurrences.cpp
+++ Hash Map & Set/1207_unique_number_of_occurrences.cpp
@@ -30,8 +30,19 @@ int main()
 {
     Solution s;
 
+    // Case 1
     std::vector<int> arr = {1,2,2,1,1,3};
     bool result = s.uniqueOccurrences(arr);
     bool expected = true;
     std::cout << "Result: " << result << ", Expected: " << expected << std::endl;
+
+    std::vector<int> arr2 = {1,2};
+    bool result2 = s.uniqueOccurrences(arr2);
+    bool expected2 = false;
+    std::cout << "Result: " << result2 << ", Expected: " << expected2 << std::endl;
+
+    std::vector<int> arr3 = {-3,0,1,-3,1,1,1,-3,10,0};
+    bool result3 = s.uniqueOccurrences(arr3);
+    bool expected3 = true;
+    std::cout << "Result: " << result3 << ", Expected: " << expected3 << std::endl;
 }
